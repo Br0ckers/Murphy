24ea6056d41e8fa1f9d93afce698e300d71575fb
Sundar Venkataraman <phi.pax@gmail.com>
2018-11-06 17:53:42 +0000

    Created the Ruby User Class - /lib/user.rb and /spec/user_spec.rb. Code nees to be refactored to ensure after the user is created the index html redirects user to login page rendered by javascript

---
+++ lib/user.rb
@@ -0,0 +1,30 @@
+class User
+	attr_reader :connection, :email, :pass
+	def initialize
+		if ENV['ENVIRONMENT'] == 'test'
+	      @connection = PG.connect(dbname: 'murphy_test')
+	    else
+	      @connection = PG.connect(dbname: 'murphy')
+	    end
+	end
+
+	def self.create(email,password)
+		user = User.new
+		user.createUser(email,password)
+	end
+
+	def createUser(email,password)
+	#	p "createUser called #{email}::#{password}"
+		prep_str = "INSERT INTO murphy_users(email,password) values('"+email+"','"+password+"');"
+		result = @connection.exec(prep_str)
+	end
+
+	def getUser(email)
+	#	p "getUser called #{email}"
+		prep_str = "SELECT user_id from murphy_users WHERE email ='"+email+"';"
+		result = @connection.exec(prep_str)
+		result.map{|item|
+			item['user_id'].to_i
+		}
+	end
+end
---
+++ spec/setup_test_database.rb
@@ -0,0 +1,12 @@
+require 'pg'
+
+def setup_test_database
+ p "setting up test murphy_test database..."
+ connection = PG.connect(dbname: 'murphy_test')
+ connection.exec('TRUNCATE murphy_users CASCADE;')
+
+ #connection.exec("INSERT INTO murphy_users(user_id,first_name,last_name,
+ #	email,password) values(1,'Joe','Bloggs','jbloggs@murphys.com','dummy');")
+ #connection.exec("INSERT INTO murphy_users(user_id,first_name,last_name,
+ #	email,password) values(2,'John','Doe','jdoe@murphys.com','dummy');")
+end
