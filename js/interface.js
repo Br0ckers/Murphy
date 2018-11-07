879ff1efe0d5302e432d4e8ba60ea2888e94dafb
Sundar Venkataraman <phi.pax@gmail.com>
2018-11-07 16:19:51 +0000

    Updated the registration screen to perform validation on Email id and Password

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   var js_user_id, js_user_email;
+  // Line below included by SVR
+  var validations ={
+  email: [/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/, 'Please enter a valid email address']
+  };
 
   $('#user-sign').hide();
   $('#addspace').hide();
@@ -10,27 +14,38 @@ $(document).ready(function() {
   })
 
   $('#signUp_submit').on('click', function() {
-    var email = $('#reg_email').val();
+    // email forced toString by SVR
+    var email = ($('#reg_email').val()).toString();
     var password = $('#reg_password').val();
     var password2 = $('#reg_password2').val();
 
-    $.ajax({
-	      type: "POST",
-	      url: "http://localhost:9292/createuser",
-	      data: {
-	        email: email,
-	        password: password
-        },
-         success:function(result,status,jqx) {
-           //console.log(result);
-           js_user_id = result;
-           js_email_id = email;
-           $('#user-reg').hide();
-           $('#bookspace').show();
-          // $(location).attr('href','/spaces');
-         }
-    });
-  });
+    // Line below included by Sundar to ensure passwords match and
+    // a valid email id is provided
+    validation = new RegExp(validations['email'][0]);
+    // validate the email value against the regular expression
+    if (!validation.test(email)){
+        // If the validation fails then we show the custom error message
+        $('#reg_messages').text(validations['email'][1]);
+    } else {
+        $('#reg_messages').text('');
+        if(validatePass(password,password2)){
+          $.ajax({
+              type: "POST",
+              url: "http://localhost:9292/createuser",
+              data: {
+                email: email,
+                password: password
+              },
+               success:function(result,status,jqx) {
+                 js_user_id = result;
+                 js_email_id = email;
+                 $('#user-reg').hide();
+                 $('#bookspace').show();
+               }
+           });
+        }
+    }
+});
 
   $('#listing_submit').on('click', function() {
     var property_name = $('#property_name').val();
@@ -46,9 +61,17 @@ $(document).ready(function() {
           price_per_night: price_per_night
 	       }
     });
-
-
-
   });
 
+  //Function created by Sundar to validate password begins
+  function validatePass(password1,password2){
+    if (password1===password2){
+      $('#reg_messages').text("");
+      return true;
+    }else{
+      $('#reg_messages').text("Password Mismatch!");
+      return false;
+    }
+  };
+ // validate password ends ---------------------------------
 });
